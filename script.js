25821f1425bd052a8560ff95e97c9affdaba658d
Jay Jayabose <jayabose@gmail.com>
2022-05-22 08:25:45 -0400

    add review and reset reviews

--- script.js
+++ script.js
@@ -13,21 +13,36 @@ console.log(URL);
 //call API and initialize movies select
 fetch(URL).
 then( (response) => { 
-    console.log(`response:  ${response}`);
+    //console.log(`response:  ${response}`);
     return response.json(); 
 }).
 then( (json) => {
-    console.log(`json:  ${json}`);
+    //console.log(`json:  ${json}`);
     movies = json;   
     initializeMoviesSelect();
-    //write movies options
 }).
 catch( (error) => {
     console.log(error);
 });
 
 
-//initialize Select People button
+//Add a review
+let form = document.querySelector("#reviewForm");
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    let userInput = event.target.review.value;    
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    span.setAttribute("class", "movieTitle");
+    span.textContent = selectedMovie.title + ': ';  
+    li.append(span);
+    li.append(userInput); //this feels clunky; is there a more elegant solution
+    let ul = document.querySelector("#reviewList");
+    ul.append(li);
+    form.reset();
+});
+
+//Show People 
 showPeople = document.querySelector("#show-people");
 showPeople.addEventListener("click", event => {
     console.log(`button click`)
@@ -35,6 +50,8 @@ showPeople.addEventListener("click", event => {
     //ol.innerHTML='';
     console.log(`selectedMovie: ${selectedMovie}`);
     
+    if (selectedMovie === undefined) return;
+
     selectedMovie.people.forEach(personURL => {
         console.log(`personURL: ${personURL}`);
         let li = document.createElement("li");
@@ -56,7 +73,12 @@ showPeople.addEventListener("click", event => {
     });
 });
 
-
+//Reset Reviews
+const resetReviews = document.querySelector("#reset-reviews");
+resetReviews.addEventListener("click", event => {
+   const reviewList = document.querySelector("#reviewList")
+   reviewList.textContent = '';
+});
 /**
  * Populates #movieSelect with movies and sets eventListeners for each
  */
@@ -95,5 +117,5 @@ const initializeMoviesSelect = () => {
 console.log(`debug`);
 
 const getMovieObj = (id) => {
-    return movies.find( element => element.id === id);
+    return movies.find(element => element.id === id);
 };
